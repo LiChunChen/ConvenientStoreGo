f8d6f79be2a85b02db80daf4ced441c408bf90d8
Satty <satty9753@gmail.com>
2017-09-27 13:17:17 +0800

    Add files via upload
    新增地圖功能

---
+++ ConvenientStoreGo-master 2/ProjectGO/shop.swift
@@ -0,0 +1,16 @@
+//
+//  shop.swift
+//  ShopMapAPI
+//
+//  Created by Michelle Chen on 2017/9/14.
+//  Copyright © 2017年 Michelle Chen. All rights reserved.
+//
+
+import Foundation
+
+class Shop{
+    var title: String!
+    var image: String!
+    var address: String!
+}
+
